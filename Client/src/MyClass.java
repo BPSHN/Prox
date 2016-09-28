18f48f44e2f0de8770506f5db503a1357ba6603b
IHaveSomeCookies <PoSeYr2504@gmail.com>
2016-09-28 11:17:19 +0300

    Выводится на экране ерунда

--- Client/src/MyClass.java
+++ Client/src/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
         System.out.println("Hello world");
         System.out.println("Здесь был Я");
         System.out.println("Hello world2");
+        System.out.println("12312");
     }
 }
