32b343b0ea44f18478713eb2da687eaccdcc204a
OtorioO <comp90194@mail.ru>
2016-09-28 10:39:52 +0300

    Мои изменения

--- Client/src/MyClass.java
+++ Client/src/MyClass.java
@@ -2,6 +2,7 @@
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Здесь был Я");
         System.out.println("Hello world2");
     }
 }
