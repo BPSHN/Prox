dcf5100500e8961ba6ec0c75e469bb295473a383
Nikitin <Sashaa70@gmail.com>
2016-11-14 14:26:25 +0300

    корректировка

---
+++ Client/src/AddContactListener.java
@@ -0,0 +1,6 @@
+/**
+ * Created by iocc on 07.11.2016.
+ */
+public interface AddContactListener {
+    public void handlerEvent (Contact contact);
+}
---
+++ Client/src/LoginMeListener.java
@@ -0,0 +1,6 @@
+/**
+ * Created by iocc on 14.11.2016.
+ */
+public interface LoginMeListener {
+    public void handlerEvent (int typeResponse);
+}
